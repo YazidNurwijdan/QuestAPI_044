7ac98d3b1cbbe3f91bf1af4221646f5d8eb56bf2
YazidNurwijdan <ynurwijdan@gmail.com>
2025-12-20 05:55:19 +0700

    mengubah isi MainActivity

---
+++ app/src/main/java/com/example/pam12/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.pam12
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pam12.ui.theme.Pam12Theme
+import com.example.pam12.uicontroller.DataSiswaApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pam12Theme() {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DataSiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
