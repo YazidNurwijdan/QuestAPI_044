82213b2c693871a38e0edc4c354b1206360451ee
YazidNurwijdan <ynurwijdan@gmail.com>
2025-12-20 03:47:27 +0700

    menambah dan mengisi DestinasiHome

---
+++ app/src/main/java/com/example/pam12/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.pam12.uicontroller.route
+
+import com.example.pam12.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
